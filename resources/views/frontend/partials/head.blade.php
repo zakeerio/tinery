5d547d5cb1915264b008ed3ea50a486efe106e3b
Zahid Ali <=>
2023-06-05 00:01:01 +0500

    updated font-awesome link

--- resources/views/frontend/partials/head.blade.php
+++ resources/views/frontend/partials/head.blade.php
@@ -25,7 +25,9 @@
 <link rel="stylesheet" href="{{ asset('css/admin/style.css') }}"> --}}
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('frontend/css/fontawesome.min.css') }}">
+{{-- <link rel="stylesheet" href="{{ asset('frontend/css/fontawesome.min.css') }}"> --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
 <link rel="stylesheet" href="{{ asset('frontend/css/mytinnery.css') }}">
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
