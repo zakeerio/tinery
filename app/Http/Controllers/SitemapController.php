512671cdc0748e397007218df2aa5e5f028cc594
Zahid Ali <=>
2023-07-06 02:26:31 +0500

    Updated Sitemap.blade, sitemapController

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -5,36 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Sitemap;
 use App\Models\Itineraries;
+use App\Models\CrudPages;
+
 
 class SitemapController extends Controller
 {
     public function index() {
         Sitemap::truncate();
 
-        $query = Itineraries::all();
-        if(!empty($query))
-        {
-            foreach($query as $row)
-            {
-                $arr = new Sitemap;
-                $arr->url = url('/').'/itineraries/'.$row->slug;
-                $arr->description = $row->title;
-                $arr->save();
-            }
-        }
-        $data = [
-            ['description' => 'Home', 'url' => url('/')],
-            ['description' => 'Itineraries', 'url' => url('/').'/itineraries'],
-            ['description' => 'Term of use', 'url' => url('/').'/term-of-use'],
-            ['description' => 'Privacy policy', 'url' => url('/').'/privacy-policy'],
-        ];
-        
-        Sitemap::insert($data);
+        $itineraries = Itineraries::all();
+        $pages = CrudPages::all();
 
-        $posts = Sitemap::all();
-        
-        return response()->view('frontend.pages.sitemap', [
-            'posts' => $posts
-        ])->header('Content-Type', 'text/xml');
+        return response()->view('frontend.pages.sitemap', compact('itineraries', 'pages'))->header('Content-Type', 'text/xml');
       }
 }
--- resources/views/frontend/pages/sitemap.blade.php
+++ resources/views/frontend/pages/sitemap.blade.php
@@ -1,11 +1,21 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    @foreach ($posts as $post)
+    @foreach ($itineraries as $itinerary)
         <url>
-            <loc>{{ $post->url }}</loc>
-            <lastmod>{{ $post->created_at->tz('UTC')->toAtomString() }}</lastmod>
+            <loc>{{ url('/').'/itineraries/'.$itinerary->slug }}</loc>
+            <lastmod>{{ $itinerary->created_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.8</priority>
         </url>
     @endforeach
+
+    @foreach ($pages as $page)
+        <url>
+            <loc>{{ url('/').$page->slug }}</loc>
+            <lastmod>{{ $page->created_at->tz('UTC')->toAtomString() }}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+    @endforeach
+
 </urlset>
