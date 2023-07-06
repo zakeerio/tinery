46f01343591fac95b41ba3c2fccefe4343c6218c
M. Umair Amjad <umairamjad52@gmail.com>
2023-07-06 22:18:56 +0500

    Update data

--- app/Imports/ItineraryLocationsImport.php
+++ app/Imports/ItineraryLocationsImport.php
@@ -23,17 +23,30 @@ class ItineraryLocationsImport implements ToCollection
             $address_country   =  $row[5];
             $latitude  =  $row[6];
             $longitude =  $row[7];
-
+        
+            $existingData = ItineraryLocations::where('address_street', $address_street)
+                ->where('address_street_line1', $address_street_line1)
+                ->where('address_city', $address_city)
+                ->where('address_state', $address_state)
+                ->where('address_zipcode', $address_zipcode)
+                ->where('address_country', $address_country)
+                ->first();
+        
+            if ($existingData) {
+                continue; // Skip if the entry already exists
+            }
+        
             $data = new ItineraryLocations;
-            $data->address_street    =  $row[0];
-            $data->address_street_line1  =  $row[1];
-            $data->address_city  =  $row[2];
-            $data->address_state =  $row[3];
-            $data->address_zipcode   =  $row[4];
-            $data->address_country   =  $row[5];
-            $data->latitude  =  $row[6];
-            $data->longitude =  $row[7];
+            $data->address_street    =  $address_street;
+            $data->address_street_line1  =  $address_street_line1;
+            $data->address_city  =  $address_city;
+            $data->address_state =  $address_state;
+            $data->address_zipcode   =  $address_zipcode;
+            $data->address_country   =  $address_country;
+            $data->latitude  =  $latitude;
+            $data->longitude =  $longitude;
             $data->save();
+            
             // Perform any necessary validation or manipulation of the data
             // For example, you can check if the email is valid or perform any transformations on the data
         }
