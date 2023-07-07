2bee4d4d9c28669311749c398b9fa62f9fd324b7
M. Umair Amjad <umairamjad52@gmail.com>
2023-07-07 20:48:03 +0500

    update data

--- app/Imports/ItineraryLocationsImport.php
+++ app/Imports/ItineraryLocationsImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\ItineraryLocations;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Illuminate\Support\Facades\DB;
 
 class ItineraryLocationsImport implements ToCollection
 {
@@ -23,32 +24,30 @@ class ItineraryLocationsImport implements ToCollection
             $address_country   =  $row[5];
             $latitude  =  $row[6];
             $longitude =  $row[7];
-        
-            $existingData = ItineraryLocations::where('address_street', $address_street)
+
+            $existingData = DB::table('itinerary_locations')
+                ->where('address_street', $address_street)
                 ->where('address_street_line1', $address_street_line1)
                 ->where('address_city', $address_city)
                 ->where('address_state', $address_state)
                 ->where('address_zipcode', $address_zipcode)
                 ->where('address_country', $address_country)
                 ->first();
-        
+
             if ($existingData) {
                 continue; // Skip if the entry already exists
             }
-        
-            $data = new ItineraryLocations;
-            $data->address_street    =  $address_street;
-            $data->address_street_line1  =  $address_street_line1;
-            $data->address_city  =  $address_city;
-            $data->address_state =  $address_state;
-            $data->address_zipcode   =  $address_zipcode;
-            $data->address_country   =  $address_country;
-            $data->latitude  =  $latitude;
-            $data->longitude =  $longitude;
-            $data->save();
-            
-            // Perform any necessary validation or manipulation of the data
-            // For example, you can check if the email is valid or perform any transformations on the data
+
+            DB::table('itinerary_locations')->insert([
+                'address_street' => $address_street,
+                'address_street_line1' => $address_street_line1,
+                'address_city' => $address_city,
+                'address_state' => $address_state,
+                'address_zipcode' => $address_zipcode,
+                'address_country' => $address_country,
+                'latitude' => $latitude,
+                'longitude' => $longitude,
+            ]);
         }
 
     }
