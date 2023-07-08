b3a7e949fa5512d54ecea8791b4e84e743603a77
M. Umair Amjad <umairamjad52@gmail.com>
2023-07-08 18:56:01 +0500

    update

--- vendor/maatwebsite/excel/config/excel.php
+++ vendor/maatwebsite/excel/config/excel.php
@@ -163,9 +163,9 @@ return [
     */
     'extension_detector' => [
         'xlsx'     => Excel::XLSX,
-        'xlsm'     => Excel::XLSX,
-        'xltx'     => Excel::XLSX,
-        'xltm'     => Excel::XLSX,
+        // 'xlsm'     => Excel::XLSX,
+        // 'xltx'     => Excel::XLSX,
+        // 'xltm'     => Excel::XLSX,
         'xls'      => Excel::XLS,
         'xlt'      => Excel::XLS,
         'ods'      => Excel::ODS,
