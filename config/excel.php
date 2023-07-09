4b79ea27efa59992f3273a7532417d3a8936b220
Zahid Ali <=>
2023-07-09 15:23:04 +0500

    Updated Xls as well in excel.php config

--- config/excel.php
+++ config/excel.php
@@ -163,20 +163,23 @@ return [
     */
     'extension_detector' => [
         'xlsx'     => 'Xlsx',
-        'xlsm'     => Excel::XLSX,
-        'xltx'     => Excel::XLSX,
-        'xltm'     => Excel::XLSX,
-        'xls'      => Excel::XLS,
-        'xlt'      => Excel::XLS,
-        'ods'      => Excel::ODS,
-        'ots'      => Excel::ODS,
-        'slk'      => Excel::SLK,
-        'xml'      => Excel::XML,
-        'gnumeric' => Excel::GNUMERIC,
-        'htm'      => Excel::HTML,
-        'html'     => Excel::HTML,
-        'csv'      => Excel::CSV,
-        'tsv'      => Excel::TSV,
+        // 'xlsm'     => Excel::XLSX,
+        // 'xltx'     => Excel::XLSX,
+        // 'xltm'     => Excel::XLSX,
+        // 'xls'      => Excel::XLS,
+        'xls'      => 'Xls',
+
+
+        // 'xlt'      => Excel::XLS,
+        // 'ods'      => Excel::ODS,
+        // 'ots'      => Excel::ODS,
+        // 'slk'      => Excel::SLK,
+        // 'xml'      => Excel::XML,
+        // 'gnumeric' => Excel::GNUMERIC,
+        // 'htm'      => Excel::HTML,
+        // 'html'     => Excel::HTML,
+        // 'csv'      => Excel::CSV,
+        // 'tsv'      => Excel::TSV,
 
         /*
         |--------------------------------------------------------------------------
